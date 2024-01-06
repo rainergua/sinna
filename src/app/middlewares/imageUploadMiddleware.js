e786d82072c6feb5bf849f60fc48133bcb71653d
rainergua <rainergua@gmail.com>
2024-01-05 22:18:23 -0400

    Adiciond e funcionalidad para subida de archivos

---
+++ src/app/middlewares/imageUploadMiddleware.js
@@ -0,0 +1,41 @@
+const multer = require('multer');
+const fs = require('fs-extra');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    let folder = '';
+    if (req.body.folder) {
+      folder = req.body.folder;
+    } else {
+      folder = 'uploads';
+    }
+    const destFolder = path.join(__dirname, '..', 'public', folder);
+    fs.ensureDirSync(destFolder);
+
+    // Set the destination folder for the uploaded file
+    cb(null, destFolder);
+  },
+  filename: (req, file, cb) => {
+    const filename = `${Date.now()}-${file.originalname}`;
+    cb(null, filename);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5 // 5 MB
+  },
+  fileFilter: (req, file, cb) => {
+    console.log(req.body)
+    console.log(file)
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('File type not supported'), false);
+    }
+  }
+});
+
+module.exports = upload;
