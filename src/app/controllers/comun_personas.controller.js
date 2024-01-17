d07ccebde78e2a6c9ae178f450c1c630e2af6fc6
Marco Antonio Quisberth Barrera <151577625+MarcoAntonioQB@users.noreply.github.com>
2024-01-17 10:41:38 -0400

    Actualización back-end servicios personas
    Servicios para la gestión de personas

---
+++ src/app/controllers/comun_personas.controller.js
@@ -0,0 +1,75 @@
+const jwt = require('jsonwebtoken');
+
+const con = require('../../infraestructure/config/config');
+
+const getParametricas = async (req, res) => {
+    try {
+        //console.log(req.user);
+        const tipo_fecha_nac = await con.query(`select * from sinna_modefa.f_combos_parametricas(18)`);
+        const ci_exp = await con.query(`select * from sinna_modefa.f_combos_parametricas(21)`);
+        const vulnerabilidad = await con.query(`select * from sinna_modefa.f_combos_parametricas(31)`);
+        const sexo = await con.query(`select * from sinna_modefa.f_combos_parametricas(15)`);
+        const instruccion = await con.query(`select * from sinna_modefa.f_combos_parametricas(231)`);
+        const e_civil = await con.query(`select * from sinna_modefa.f_combos_parametricas(244)`);
+        const ocupacion = await con.query(`select * from sinna_modefa.f_combos_parametricas(249)`);
+        const vive_con = await con.query(`select * from sinna_modefa.f_combos_parametricas(299)`);
+
+        res.status(200).json({
+            datoAdicional: {
+                tipo_fecha_nac: tipo_fecha_nac.rows,
+                ci_exp: ci_exp.rows,
+                vulnerabilidad: vulnerabilidad.rows,
+                sexo: sexo.rows,
+                instruccion: instruccion.rows,
+                e_civil: e_civil.rows,
+                ocupacion: ocupacion.rows,
+                vive_con: vive_con.rows
+            },
+            mensaje:"Parámétricas obtenidas con éxito",
+            cod:200}
+        );
+    } catch (e) {
+        res.status(500).json({ msg: 'Error: ' + e });
+    }
+}
+
+// Gestión de las personas
+const gestionPersona = async (req, res) => {
+    const v_json = req.body
+
+    const query = {
+        text: `call comun.p_personas($1) `,
+        values:[v_json]        
+    };
+    await con
+        .query(query)
+        .then((result) =>{
+            const resultado =  result.rows;
+            res.status(200).json({
+                datoAdicional: resultado,
+                mensaje:"Gestión de personas establecida con éxito",
+                cod:200
+            })}
+        )
+        .catch((e) => res.status(500).json({ msg: 'Error:'+ e }))
+}
+
+// Listar a las personas registradas en el SINNA
+const mostrarPersona = async (req, res) => {
+    try {
+        const listado = await con.query(`select * from sinna_modefa.f_mostrar_personas()`);
+        res.status(200).json({ 
+            datoAdicional: listado.rows,
+            mensaje:"Lista de personas registradas en el SINNA",
+            cod:200 
+        });
+    } catch (e) {
+        res.status(500).json({ msg: 'Error: ' + e });
+    }
+}
+
+module.exports = {
+    getParametricas, 
+    gestionPersona,
+    mostrarPersona
+}
---
+++ src/app/routes/comun_personas.routes.js
@@ -0,0 +1,197 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const { 
+    getParametricas,
+    gestionPersonas,
+    mostrarPersonas,
+    gestionPersona,
+    mostrarPersona,
+} = require ('../controllers/comun_personas.controller')
+
+/**
+ * @swagger
+ * securityDefinitions:
+ *   bearerAuth:
+ *     type: apiKey
+ *     name: Authorization
+ *     in: header
+ */
+/**
+ * @swagger
+ * /api/persona-parametricas:
+ *  get:
+ *      summary: Obtiene las parametricas para el registro de personas
+ *      tags: [personas]
+ *      security:
+ *          - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: Petición Exitosa
+ * 
+ */
+router.get('/persona-parametricas',
+    passport.authenticate('jwt', {session:false}),
+    getParametricas
+);
+
+/**
+ * @swagger
+ * components:
+ *  schemas:    
+ *      com_personas:
+ *          type: object
+ *          properties:
+ *              id_persona:
+ *                  type: integer
+ *                  description: ID Persona
+ *              convencional:
+ *                  type: boolean
+ *                  description: NNA Convencional
+ *              primer_apellido:
+ *                  type: string
+ *                  description: Primer apellido de la persona
+ *              segundo_apellido:
+ *                  type: string
+ *                  description: Segundo apellido de la persona
+ *              nombres:
+ *                  type: string
+ *                  description: Nombres de la persona
+ *              tipo_fecha_nac:
+ *                  type: integer
+ *                  description: Tipo de fecha de nacimiento
+ *              fecha_nac:
+ *                  type: date
+ *                  description: Fecha de nacimiento
+ *              lugar_nac:
+ *                  type: string
+ *                  description: Lugar de nacimiento
+ *              ci:
+ *                  type: integer
+ *                  description: Cédula de identidad
+ *              ci_ext:
+ *                  type: string
+ *                  description: Cédula de identidad (extensión)
+ *              ci_exp:
+ *                  type: integer
+ *                  description: Cédula de identidad (expedición)
+ *              vulnerabilidad:
+ *                  type integer
+ *                  description: Situación de vulnerabilidad de la persona
+ *              direccion:
+ *                  type: string
+ *                  description: Dirección de la persona
+ *              latitud:
+ *                  type: string
+ *                  description: Latitud de la dirección de la persona
+ *              longitud:
+ *                  type: string
+ *                  description: Longitud de la dirección de la persona
+ *              telefono:
+ *                  type: string
+ *                  description: Número telefónico de la persona
+ *              celular:
+ *                  type: string
+ *                  description: Número de celular de la persona
+ *              fotografia:
+ *                  type: string
+ *                  description: Fotografía de la persona
+ *              sexo:
+ *                  type: integer
+ *                  description: Género de la persona
+ *              segip:
+ *                  type: boolean
+ *                  description: Persona registrada en el SEGIP
+ *              instruccion:
+ *                  type: integer
+ *                  description: Nivel de instrucción de la persona
+ *              e_civil:
+ *                  type: integer
+ *                  description: Estado civil de la persona
+ *              ocupacion:
+ *                  type: integer
+ *                  description: Ocupación de la persona
+ *              vive_con:
+ *                  type: integer
+ *                  description: Con quien vive la persona
+ *              transacción:
+ *                  type: string
+ *                  description: CREAR_DEF,EDITAR_DEF,ELIMINAR_DEF,RESTAURAR_DEF
+ *              estado:
+ *                  type: string
+ *                  description: ACTIVO o INACTIVO
+ *              login:
+ *                  type: string
+ *                  description: el login del usuario
+ *          required:
+ *              - transaccion
+ *              - estado
+ *              - login
+ *          example:
+ *              id_persona: 4
+ *              convencional: false
+ *              primer_apellido: Bautista
+ *              segundo_apellido: Montaño
+ *              nombres: Jaime
+ *              tipo_fecha_nac: 19
+ *              fecha_nac: 2014-09-22
+ *              lugar_nac: "El Alto, La Paz"
+ *              ci: 9999999
+ *              ci_ext: null
+ *              ci_exp: 3
+ *              vulnerabilidad: 230
+ *              direccion: "Av. 6 de marzo entre calles 5 y 6. V. Dolores"
+ *              latitud: -16.510169
+ *              longitud: -68.164667
+ *              telefono: 2919191
+ *              celular: null
+ *              fotografia: null
+ *              sexo: 16
+ *              segip: false
+ *              instruccion: null
+ *              e_civil: null
+ *              ocupacion: null
+ *              vive_con: null
+ *              id_creado_por: 2 
+ *              fecha_creado: "2024-01-11 20:14:41.994237"
+ *              id_modificado_por: null
+ *              fecha_modificado: null
+ *              estado: CREADO
+ *              transaccion: CREAR_PERSONA
+ *              login: 1
+ */
+/**
+ * @swagger
+ * /api/persona:
+ *  post:
+ *      summary: Envía los datos para la gestión de personas (ABM). El objeto en el request debe llamarse <v_json>
+ *      tags: [personas]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/personas'
+ *      responses:
+ *          200:
+ *              description: Peticion Exitosa
+ * 
+ */
+router.post('/persona', gestionPersona);
+
+/**
+ * @swagger
+ * /api/mostrar-persona:
+ *  post:
+ *      summary: lista de personas registradas en el SINNA
+ *      tags: [personas]
+ *      responses:
+ *          200:
+ *              description: Peticion Exitosa
+ * 
+ */
+router.post('/mostrar-persona', mostrarPersona);
+
+
+module.exports = router;
